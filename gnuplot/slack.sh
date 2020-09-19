076ba29043b2478dbef9ba73aa24de62cf39cf75
yyh-gl <yhonda.95.gl@gmail.com>
2020-09-19 12:41:47 +0900

    気温情報のSlack通知機能を本実装

--- gnuplot/slack.sh
+++ gnuplot/slack.sh
@@ -1,10 +1,10 @@
-curl -X POST $WEBHOOK_URL \
+curl -X POST $WEBHOOK_URL_TO_02 \
      -H 'Content-Type: application/json; charset=UTF-8' \
      -d '{
     "attachments": [
       {
-          "text": "Hello world :tada:",
-          "image_url": "https://growthseed.jp/wp-content/uploads/2016/12/peach-1.jpg"
+          "text": "気温レポート",
+          "image_url": "https://super.hobigon.work/public/temperature.png"
       }
   ]
 }'
